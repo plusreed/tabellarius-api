c7470c5ee2b38544bad92e5886cb6c89be7d6f73
Reed Davenport <reed@plusreed.com>
2022-09-18 00:21:09 -0400

    remove unused fs import

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const http = require('http')
 const { Server } = require('socket.io')
-const fs = require('fs/promises')
 const path = require('path')
 const readline = require('readline')
 
